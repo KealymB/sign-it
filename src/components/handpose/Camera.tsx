102fde83c5fb6c9a6b17dbb1a2fd808444bad62c
Kealym Bromley <kealymmb@gmail.com>
2023-10-15 18:20:16 +0200

    Added wireframe handpose

---
+++ src/components/handpose/Camera.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from "react";
+import "@tensorflow/tfjs-backend-webgl";
+
+import * as handdetection from "@tensorflow-models/hand-pose-detection";
+
+import Webcam from "react-webcam";
+import { drawHand2 } from "@/utils/handpose/drawHand2";
+
+const Camera = () => {
+  const detectorFps = 30;
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [model, setModel] = useState<handdetection.HandDetector>();
+
+  useEffect(() => {
+    if (!model) {
+      loadModel().catch((error) => {
+        console.log(error);
+      });
+    }
+    if (model) {
+      runHandpose();
+    }
+  }, [model]);
+
+  const loadModel = async () => {
+    const handPoseModel = await handdetection.createDetector(
+      handdetection.SupportedModels.MediaPipeHands,
+      {
+        runtime: "tfjs",
+        maxHands: 2,
+        modelType: "full",
+      },
+    );
+    setModel(handPoseModel);
+    console.log("Handpose model loaded.");
+  };
+
+  const runHandpose = () => {
+    console.log("setup detector");
+
+    const detectInterval = async () => {
+      await detectHands();
+    };
+
+    const intervalId = setInterval(detectInterval, (1 / detectorFps) * 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  };
+
+  const detectHands = async () => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      typeof canvasRef.current !== "undefined" &&
+      canvasRef.current !== null &&
+      webcamRef.current?.video !== null &&
+      webcamRef.current?.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      // setting canvas dimensions
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //DETECT HAND
+      const hand = await model?.estimateHands(video);
+      const ctx = canvasRef.current.getContext("2d");
+
+      if (hand && ctx) {
+        drawHand2(hand, ctx);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-1">
+      <header className="relative flex-1">
+        <Webcam className="flex flex-1" ref={webcamRef} />
+        <canvas className="absolute left-0 top-0 flex flex-1" ref={canvasRef} />
+      </header>
+    </div>
+  );
+};
+
+export default Camera;
--- src/pages/realtime/index.tsx
+++ src/pages/realtime/index.tsx
@@ -1,10 +1,13 @@
+import Camera from "@/components/handpose/Camera";
+
 export default function RTDetection() {
   return (
-    <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+    <main className=" flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="container flex flex-col items-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Real <span className="text-[hsl(280,100%,70%)]">Time</span>
         </h1>
+        <Camera />
       </div>
     </main>
   );
